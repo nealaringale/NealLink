85ac6d4fbee02470972d7f8fa8b8b1ded240149b
Neal Aringale <123873477+nealaringale@users.noreply.github.com>
2026-08-20 21:52:35 +0530

    Pin Android JVM toolchain to Java 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,17 @@ android {
         applicationId = "com.neallink.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
